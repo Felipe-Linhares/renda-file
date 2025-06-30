738c24376ce47f1ff9b892302d0933f57754c95b
Felipe-Linhares <jose.felipe.ribeiro08@aluno.ifce.edu.br>
2025-06-30 20:02:07 -0300

    feat: configurar projeto para deploy no GitHub Pages com catálogo de renda filé completo

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('postcss-load-config').Config} */
+const isProd = process.env.NODE_ENV === "production";
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 const config = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      ...(isProd &&
+        isGitHubPages && {
+          basePath: "/renda-file-catalogo",
+        }),
+    },
     autoprefixer: {},
   },
 };
